3c008974545777778749f86a697545cf84f6aa9d
Alfredo Rodriguez <fredd.073100@gmail.com>
2022-02-09 15:58:43 -0600

    Registro fusionado con el proyecto

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,41 @@
+const bcrypt = require("bcryptjs");
+const User = require("../../src/models/UserModel");
+const { validationResult } = require("express-validator");
+
 module.exports = {
   register: (req, res) => res.render("register"),
   login: (req, res) => res.render("login"),
+  registroProceso: function (req, res) {
+    const resultadoValidacion = validationResult(req);
+
+    if (resultadoValidacion.errors.length > 0) {
+      return res.render("register", {
+        errors: resultadoValidacion.mapped(),
+        oldData: req.body,
+      });
+    }
+
+    if (req.body.contraseña != req.body.Repitepassword) {
+      return res.render("register", {
+        errors: { Repitepassword: { msg: "Las contraseñas no coinciden" } },
+      });
+    }
+
+    let usuarioCreado = {
+      ...req.body,
+      contraseña: bcrypt.hashSync(req.body.contraseña, 10),
+    };
+
+    let usuarioExistente = User.filtrarCampo("email", req.body.email);
+    if (usuarioExistente) {
+      return res.send("usuario existente");
+      //return res.render("register", {
+
+      //errors: { email: {msg: "este email ya esta registrado"}, }
+      //})
+    }
+
+    User.crearUsuario(usuarioCreado);
+    res.send("usuario creado");
+  },
 };
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -2,8 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const mainController = require("../controllers/userController");
+const validaciones = require("../middlewares/registroMiddleWare");
 
 router.get("/register", mainController.register);
 router.get("/login", mainController.login);
+
+router.post("/register", validaciones, mainController.registroProceso);
 
 module.exports = router;
